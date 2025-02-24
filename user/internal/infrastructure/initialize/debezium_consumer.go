752b6bbaab98d8ac3d3adbf0410442f8f6c82576
Banh Hao Toan <toan@aviansolutions.aero>
2025-02-24 13:23:26 +0700

    fix cdc

--- user/internal/infrastructure/initialize/debezium_consumer.go
+++ user/internal/infrastructure/initialize/debezium_consumer.go
@@ -4,83 +4,131 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
 
 	"github.com/bhtoan2204/user/global"
+	"github.com/bhtoan2204/user/internal/domain/entities"
+	"github.com/bhtoan2204/user/internal/domain/repository/query"
+	"github.com/bhtoan2204/user/internal/infrastructure/db/elasticsearch/repository"
 	"github.com/segmentio/kafka-go"
 	"go.uber.org/zap"
 )
 
 type DebeziumMessage struct {
-	Source struct {
-		Database string `json:"db"`
-		Table    string `json:"table"`
-	} `json:"source"`
 	Payload struct {
 		Before json.RawMessage `json:"before"`
 		After  json.RawMessage `json:"after"`
-		Op     string          `json:"op"` // "c", "u", "d"
+		Source struct {
+			Version   string `json:"version"`
+			Connector string `json:"connector"`
+			Name      string `json:"name"`
+		} `json:"source"`
+		Transaction json.RawMessage `json:"transaction"`
+		Op          string          `json:"op"`
+		TsMs        int64           `json:"ts_ms"`
+		TsUs        int64           `json:"ts_us"`
+		TsNs        int64           `json:"ts_ns"`
 	} `json:"payload"`
 }
 
-func processDebeziumMessage(msg kafka.Message) {
-	global.Logger.Info("Processing message", zap.String("key", string(msg.Key)))
+type rawUser struct {
+	ID           uint   `json:"id"`
+	CreatedAt    int64  `json:"created_at"`
+	UpdatedAt    int64  `json:"updated_at"`
+	DeletedAt    *int64 `json:"deleted_at"` // pointer để nhận null
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Phone        string `json:"phone"`
+	BirthDate    int64  `json:"birth_date"`
+	Address      string `json:"address"`
+	PasswordHash string `json:"password_hash"`
+	PinCode      string `json:"pin_code"`
+	Status       int    `json:"status"`
+}
 
+type Debezium struct {
+	reader           *kafka.Reader
+	esUserRepository query.ESUserRepository
+}
+
+func NewDebezium(esRepo query.ESUserRepository) *Debezium {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{global.Config.KafkaConfig.Broker},
+		GroupID: "inventory-connector",
+		Topic:   "dbserver1.user.users",
+	})
+	return &Debezium{
+		reader:           reader,
+		esUserRepository: esRepo,
+	}
+}
+
+func (d *Debezium) ProcessMessage(msg kafka.Message) {
+	global.Logger.Info("Processing message")
 	var message DebeziumMessage
-
 	if err := json.Unmarshal(msg.Value, &message); err != nil {
-		fmt.Println("Error unmarshalling message", err)
+		global.Logger.Error("Error unmarshalling message", zap.Error(err))
 		return
 	}
-
+	fmt.Println(string(message.Payload.After))
 	switch message.Payload.Op {
 	case "c", "u":
-		global.Logger.Info("Create or Update operation")
-		var data map[string]interface{}
-		if err := json.Unmarshal(message.Payload.After, &data); err != nil {
-			global.Logger.Error("Error unmarshalling user data", zap.Error(err))
+		var ru rawUser
+		if err := json.Unmarshal(message.Payload.After, &ru); err != nil {
+			global.Logger.Error("Error unmarshalling raw user data", zap.Error(err))
 			return
 		}
 
-		docID := fmt.Sprintf("%v", data["id"])
-		res, err := global.ESClient.Index(
-			message.Source.Table, // index name in ES
-			strings.NewReader(string(message.Payload.After)),
-			global.ESClient.Index.WithDocumentID(docID),
-			global.ESClient.Index.WithContext(context.Background()),
-		)
-		if err != nil {
+		var user entities.User
+		user.AbstractModel = entities.AbstractModel{
+			ID: uint(ru.ID),
+			// CreatedAt: ru.CreatedAt,
+			// UpdatedAt: ru.UpdatedAt,
+		}
+		user.Username = ru.Username
+		user.Email = ru.Email
+		user.FirstName = ru.FirstName
+		user.LastName = ru.LastName
+		user.Phone = ru.Phone
+		user.Address = ru.Address
+		user.PasswordHash = ru.PasswordHash
+		user.PinCode = ru.PinCode
+		user.Status = 1
+		user.BirthDate = nil
+		// if ru.BirthDate > 0 {
+		// 	t := time.Unix(0, ru.BirthDate*int64(time.Millisecond))
+		// 	user.BirthDate = &t
+		// } else {
+		// 	user.BirthDate = nil
+		// }
+
+		if err := d.esUserRepository.Index(&user); err != nil {
 			global.Logger.Error("Error indexing document", zap.Error(err))
 			return
 		}
-		res.Body.Close()
 		global.Logger.Info("Document indexed successfully")
 	default:
 		global.Logger.Info("No operation")
 	}
 }
 
-func ConsumeCDC() {
+func (d *Debezium) Consume() {
 	for {
-		fmt.Println("Waiting for message")
-		m, err := global.KafkaDebeziumConsumer.ReadMessage(context.Background())
-		fmt.Println("Message received", string(m.Value))
+		global.Logger.Info("Waiting for message")
+		m, err := d.reader.ReadMessage(context.Background())
 		if err != nil {
-			fmt.Println("Error reading message", err)
+			global.Logger.Error("Error reading message", zap.Error(err))
 			continue
 		}
-
-		processDebeziumMessage(m)
+		global.Logger.Info("Message received")
+		d.ProcessMessage(m)
 	}
 }
 
-// name: mysql-connector
 func InitDebeziumConsumer() {
-	global.KafkaDebeziumConsumer = kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{global.Config.KafkaConfig.Broker},
-		GroupID: "inventory-connector",
-		Topic:   "dbserver1.user.users",
-	})
 
-	go ConsumeCDC()
+	esRepo := repository.NewESUserRepository(global.ESClient)
+	debezium := NewDebezium(esRepo)
+	go debezium.Consume()
 }
