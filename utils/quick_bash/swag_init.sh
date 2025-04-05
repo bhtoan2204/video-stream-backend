87c471c9c8bcd113532549de43e2cfc6596f51cc
Nam Dao <86637323+bhtoan2204@users.noreply.github.com>
2025-04-05 17:54:52 +0700

    remove swag_init.sh script; add generate-swagger target to Makefile for documentation generation

--- utils/quick_bash/swag_init.sh
+++
@@ -1,3 +0,0 @@
-# /bin/bash
-cd ../apps/gateway/cmd/main
-swag init --parseDependency -g main.go -d .,../../internal/modules/auth/handler,../../internal/modules/user/handler,../../internal/modules/comment/handler,../../internal/modules/video/handler
