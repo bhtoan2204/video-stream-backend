f0fb9b14def63892831839edb9d74abd5de4885b
Nam Dao <86637323+bhtoan2204@users.noreply.github.com>
2025-04-22 01:59:35 +0700

    stuff

--- infra/k8s/installation/install_kafka.sh
+++ infra/k8s/installation/install_kafka.sh
@@ -2,13 +2,13 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 kubectl create namespace kafka
 
-helm install kafka bitnami/kafka \
+helm upgrade --install kafka bitnami/kafka \
   --namespace kafka \
-  --set kraft.enabled=true \
-  --set replicaCount=3 \
-  --set controller.replicaCount=3 \
-  --set externalAccess.enabled=false \
-  --set listeners.client.protocol=PLAINTEXT \
-  --set storageClass=local-path \
-  --set persistence.size=8Gi
+  --create-namespace \
+  --set persistence.size=8Gi,logPersistence.size=8Gi,volumePermissions.enabled=true,persistence.enabled=true,logPersistence.enabled=true,serviceAccount.create=true,rbac.create=true \
+  --version 23.0.7 \
+  -f kafka-values.yaml
+
+
+
 
